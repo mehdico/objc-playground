bb029e7434d54e00dba875d6260d456401f6f20c
Mehdi Mousavi <malemir@gmail.com>
2021-11-10 11:44:13 +0330

    Add files

---
+++ objc_playground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc_playground
+//
+//  Created by Mehdico on November 5, 2019.
+//  Copyleft © Mehdico.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
